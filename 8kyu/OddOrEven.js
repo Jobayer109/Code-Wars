596b0498edbac37d6ded81b5a9d2b9746b747a40
user.email <jobayer.ahmed109@gmail.com>
2023-09-30 21:26:28 +0600

    Sum of positive number of an array.

--- 8kyu/OddOrEven.js
+++ 8kyu/OddOrEven.js
@@ -7,3 +7,16 @@
 function evenOrOdd(number) {
   return number % 2 === 0 ? "Even" : "Odd";
 }
+
+// //
+
+// function mathPow(num) {
+//   return Math.pow(String(num).split(""), 2);
+// }
+
+// console.log(mathPow(12));
+
+// console.log(Math.pow(String(12).split(""), 2).toString());
+
+const num = 25;
+// console.log(Number(num.toString().split("")));
---
+++ 8kyu/SumOfPositive.js
@@ -0,0 +1,38 @@
+/*
+    Sum of positive number of an array.
+*/
+
+// Solution: 1 _________________________________
+const array = [1, -4, 7, 12];
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+const result = positiveSum(array); // 20
+
+// --------------------------------------------------------------------->>
+
+// Solution: 2 _________________________________
+const positiveSum2 = (arr) => {
+  return arr.filter((value) => value > 0).reduce((a, b) => a + b, 0);
+};
+
+positiveSum2(array);
+
+// --------------------------------------------------------------------->>
+
+// Solution: 3 _________________________________
+function positiveSum3(arr) {
+  let sum = 0;
+  for (let v of arr) {
+    sum += v;
+  }
+  return sum;
+}
+positiveSum([12, -8, 8, 10]); // 30
